2a7699d4d096c3bdabc1699fe1eb450373ba30dd
Jimting <handsome841206@gmail.com>
2020-05-03 12:27:11 +0800

    Update shop_item_crawler.js

--- scripts/shop_item_crawler.js
+++ scripts/shop_item_crawler.js
@@ -33,6 +33,7 @@ module.exports = function(robot)
 			if (!error && res.statusCode == 200) 
 			{
 				// 將拿到的最新結果進行分析與儲存，並將有更動的資料加到MessageQueue上。
+				response.reply("總共搜尋到"+body.length+"筆商品資料。")
 				analyseSearchResult(body, keywords);
 			}
 			if(error)
