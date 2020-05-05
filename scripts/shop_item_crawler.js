efdafaef1ce279633aed37fc011236d486fd619e
Jimting <handsome841206@gmail.com>
2020-05-05 23:08:24 +0800

    Update shop_item_crawler.js

--- scripts/shop_item_crawler.js
+++ scripts/shop_item_crawler.js
@@ -196,7 +196,7 @@ async function analyseSearchResult(data_json, keywords)
 	{
 		console.log("=====第"+i+"筆資料=====");
 		const itemExistStatus = await ifItemExist(data_json[i].link)
-		.then(await checkItemExist(item_json, itemExistStatus, keywords););
+		.then(checkItemExist(item_json, itemExistStatus, keywords));
 	} 
 }
 
