8c6b58e1919044e2fcad9f307926dd15f6a075e5
jimting <handsome841206@gmail.com>
2020-05-19 22:34:51 +0800

    Update shop_item_crawler.js

--- scripts/shop_item_crawler.js
+++ scripts/shop_item_crawler.js
@@ -489,7 +489,7 @@ function followItemsCronJob(robot)
 			cron_keyword_list.push(keyword);
 		}
 		
-		cronjob = cron.schedule("00 34 * * * *", function(){
+		cronjob = cron.schedule("00 40 * * * *", function(){
 				
 			startCronSearch(robot, cron_keyword_list, 0);
 		});
@@ -503,7 +503,7 @@ function startCronSearch(robot, cron_keyword_list, k) {
   setTimeout(function() {
     console.log("---------------------");
 	console.log("Running Cron Job, keyword : " + cron_keyword_list[k]);
-	robot.messageRoom("831516917", Date.now()+" | 現在是整點了！開始查詢！關鍵字："+cron_keyword_list[k]);
+	robot.messageRoom("831516917", Date.now()+" | 開始查詢！關鍵字："+cron_keyword_list[k]);
 	shopeeCrawler(cron_keyword_list[k]); 
     if (k < cron_keyword_list.length) {           
       myLoop(robot, cron_keyword_list, k+1); 
